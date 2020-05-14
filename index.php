df700ab9017f7482948b18aadec49ededef16653
NIKHIL GUPTA <guptanikhil719@gmail.com>
2020-05-15 00:39:17 +0530

    test2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 print 'TELL ME ABOUT YOURSELF';
-print '---**---MY NAME IS NIKHIL---**---';
-print '--**--I AM AN ENGINEER--**--';
+prinnt '---**---MY NAME IS NIKHIL---**---';
+printt '--**--I AM AN ENGINEER--**--';
 ?>
